c46f173ea2a164da3bf31c1ea72d7350d66fdc9b
Scott Block <scott.block@hungrymachine.com>
2012-08-14 14:44:21 -0400

    better format

--- public/js/question.js
+++ public/js/question.js
@@ -1,12 +1,21 @@
 function QuestionCtrl($scope){
+    $scope.types = [
+      {val: 'rap', display: 'DC Rap'},
+      {val: 'tech', display: 'DC Tech'}
+    ]
+  
     $scope.questions = [
-      {text: 'DC Over Everything', possible_authors: ['Wale', 'Doug'], correct_author: 'Wale'},
-      {text: 'Just Ship It', possible_authors: ['Wale', 'Doug'], correct_author: 'Doug'}
+      {text: 'Ambition is priceless. It\'s something that\'s in your veins.', correct_type: 'rap'},
+      {text: 'I feel every day that I can conquer the world, and I have no qualms about that.', correct_type: 'tech'}
     ];
     
     //make each question initially incorrect and not tried
     $.each($scope.questions, function(){
-      _.defaults(this, {correct: false, tried: false});
+      _.defaults(this, {
+        correct: false, 
+        tried: false,
+        extra: ''
+      });
     });
     
     $scope.getCorrectAnswers = function(){
@@ -22,15 +31,21 @@ function QuestionCtrl($scope){
     
     $scope.checkAnswer = function(){
       var question = this.$parent.question;
-      if(this.a == question.correct_author && question.tried == false){
+      if(this.a.val == question.correct_type && question.tried == false){
         this.question.correct = true;
         this.question.tried = true;
       }
       this.question.tried = true;
       
+      $scope.addExtra(this.$index, question)
       $scope.checkIfDone();
       
     };
+    
+    $scope.addExtra = function(index, question){
+      var content = $($('#extras li').get(index)).html();
+      question.extra = content;
+    }
     
     $scope.checkIfDone = function(){
       var done = true;
