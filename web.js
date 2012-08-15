d99db7dbc3b3d558d313e104f36b096f78d2511e
Scott Block <scott.block@hungrymachine.com>
2012-08-15 13:54:19 -0400

    back to oauth

--- web.js
+++ web.js
@@ -6,45 +6,7 @@
 var express = require('express'),
     routes = require('./routes'),
     mongoose = require('mongoose'),
-    mongooseAuth = require('mongoose-auth'),
-    everyauth = require('everyauth'),
-    Promise = everyauth.Promise;
-
-//mongo
-var db_url = process.env.MONGOHQ_URL || 'mongodb://localhost/test'
-mongoose.connect(db_url);
-
-var Schema = mongoose.Schema
-  , ObjectId = Schema.ObjectId;
-
-var UserSchema = new Schema({
-    id        : String
-  , score     : Number
-});
-
-UserSchema.plugin(mongooseAuth, {
-  everymodule: {
-    everyauth: {
-      User: function() {
-        return User;
-      }
-    }
-  },
-
-  twitter: {
-    everyauth: {
-      myHostname: 'http://dctechdcrap.herokuapp.com/',
-      consumerKey: 'dfrfRrhx2uIYXvwPGH3sPg',
-      consumerSecret: 'zKLEOknTpb5e0ZzlWXcdU19nJj2RkcvCLqO9wQVr70',
-      redirectPath: '/'
-    }
-  }
-});
-
-
-mongoose.model('User', UserSchema); 
-User = mongoose.model('User');
-
+    OAuth = require('oauth').OAuth;
     
 var app = module.exports = express.createServer();
 
@@ -60,7 +22,8 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
   app.use(express.cookieParser()); 
   app.use(express.session({secret: 'dctech'}));
-  app.use(mongooseAuth.middleware());
+
+
   app.use(app.router);
 });
 
@@ -82,10 +45,72 @@ app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
-//Twitter Oauth
-app.get('/leaderboard', function(req, res){
-  res.render('leaderboard')
+//mongo
+var db_url = process.env.MONGOHQ_URL || 'mongodb://localhost/test'
+mongoose.connect(db_url);
+
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId;
+  
+var UserSchema = new Schema({
+    id        : String
+  , score     : Number
 });
+
+mongoose.model('User', UserSchema); 
+var User = mongoose.model('User');
+
+//Twitter Oauth
+var oa = new OAuth(
+	"https://api.twitter.com/oauth/request_token",
+	"https://api.twitter.com/oauth/access_token",
+	"dfrfRrhx2uIYXvwPGH3sPg",
+	"zKLEOknTpb5e0ZzlWXcdU19nJj2RkcvCLqO9wQVr70",
+	"1.0",
+	"http://dctechdcrap.herokuapp.com/auth/twitter/callback",
+	"HMAC-SHA1"
+);
+
+app.get('/auth/twitter', function(req, res){
+	oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
+		if (error) {
+			console.log(error);
+			res.send("yeah no. didn't work.")
+		}
+		else {
+			req.session.oauth = {};
+			req.session.oauth.token = oauth_token;
+			console.log('oauth.token: ' + req.session.oauth.token);
+			req.session.oauth.token_secret = oauth_token_secret;
+			console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
+			res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
+	}
+	});
+});
+
+app.get('/auth/twitter/callback', function(req, res, next){
+	if (req.session.oauth) {
+		req.session.oauth.verifier = req.query.oauth_verifier;
+		var oauth = req.session.oauth;
+
+		oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier, 
+		function(error, oauth_access_token, oauth_access_token_secret, results){
+			if (error){
+				console.log(error);
+				res.send("yeah something broke.");
+			} else {
+				req.session.oauth.access_token = oauth_access_token;
+				req.session.oauth,access_token_secret = oauth_access_token_secret;
+				console.log(results);
+				res.send("worked. nice one.");
+			}
+		}
+		);
+	} else
+		next(new Error("you're not supposed to be here."))
+});
+
+
 
 
 
