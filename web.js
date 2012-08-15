06df60e8dce940ae6963bda75aff4fffc1e2983b
Scott Block <scott.block@hungrymachine.com>
2012-08-15 09:51:13 -0400

    port switch

--- web.js
+++ web.js
@@ -43,6 +43,6 @@ app.get('*', routes.index);
 
 // Start server
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
