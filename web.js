6944f18cbb89d2d4ef472c89410254214cc25366
Scott Block <scott@scottblock.com>
2012-08-17 00:04:57 -0400

    body

--- web.js
+++ web.js
@@ -1,20 +1,4 @@
-function censor(censor) {
-  return (function() {
-    var i = 0;
 
-    return function(key, value) {
-      if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value) 
-        return '[Circular]'; 
-
-      if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
-        return '[Unknown]';
-
-      ++i; // so we know we aren't using the original object anymore
-
-      return value;  
-    }
-  })(censor);
-}
 /**
  * Module dependencies.
  */
@@ -55,8 +39,8 @@ app.get('/partials/:name', routes.partials);
 
 //get score post
 app.post('/user/new', function(req, res) {
-    console.log(req);
-    res.send('Data received: ' + JSON.stringify(req, censor(req)));
+    console.log(req.body);
+    res.send('Data received: ' + JSON.stringify(req.body));
         
     /*
          var saveUser = new User(); 
