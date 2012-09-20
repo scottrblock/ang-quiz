fa5e37eec3bd5ae96268075f46244edfb007c3da
Scott Block <sblock@Scotts-MacBook-Pro.local>
2012-09-19 22:32:38 -0400

    iterate

--- public/js/user.js
+++ public/js/user.js
@@ -3,15 +3,19 @@ function UserCtrl($scope, $http){
   $scope.users = function(){
     var users = [];
     $http.get('/user/list.json').success(function(data, status){
+      console.log("data " + data);
       new_users = data;
+      console.log("new_users " + new_users);
+
       sorted_users = _.sortBy(new_users, function(user){
         return (0 - user.score); 
       });
+      console.log("sorted_users " + sorted_users);
       
-      users = sorted_users
+      users = sorted_users;
     });
-
-    return sorted_users;
+    
+    return users;
   }
    
 }
